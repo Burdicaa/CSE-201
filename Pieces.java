a2f2ba4a731f14f6e4c1e8c63a20ef3a1d47c782
yangj90-gif <yangj90@miamioh.edu>
2025-09-26 10:09:28 -0400

    Add files via upload

---
+++ Pieces.java
@@ -0,0 +1,93 @@
+
+public class Pieces {
+	private int year;
+	private String name;
+	private String museum;
+	private int worth;
+	private String description;
+	private String creator;
+	private String image;
+	private String[] comments;
+	private int commentcount;
+	
+	public Pieces(int year, String name, String museum, int worth, String description, 
+			String creator, String image) {
+		this.year = year;
+		this.name = name;
+		this.museum = museum;
+		this.worth = worth;
+		this.description = description;
+		this.creator = creator;
+		this.image = image;
+		
+		comments = new String[10];
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getMuseum() {
+		return museum;
+	}
+	
+	public int getWorth() {
+		return worth;
+	}
+	
+	public String getDecription() {
+		return description;
+	}
+	
+	public String getCreator() {
+		return creator;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public String[] getComments() {
+		return comments;
+	}
+	
+	public int getCommentCount() {
+		return commentcount;
+	}
+	
+	public String getComment(int index) {
+		return comments[index];
+	}
+	
+	public void addComments(String s) {
+		if (commentcount == comments.length) {
+			String[] temp = new String[comments.length * 2];
+			for (int i = 0; i < comments.length; i++) {
+				temp[i] = comments[i];
+			}
+			comments = temp;
+		}
+		comments[commentcount] = s;
+		commentcount++;
+	}
+	
+	public void removeComments(int n) {
+		for (int i = 0; i < commentcount; i++) {
+			if (comments[i] == comments[n]) {
+				comments[i] = comments[commentcount - 1];
+				comments[commentcount - 1] = null;
+				commentcount--;
+			}
+		}
+	}
+	
+	public String toString() {
+		String f = String.format("\nName: %s \nCreator: %s \nYear: %d \nMuseum: %s \nWorth: $%d"
+				+ " \nDescription: %s", name, creator, year, museum, worth, description);
+		return f;
+	}
+}
