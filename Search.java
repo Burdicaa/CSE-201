c2cb7e924b48d679acb1b6b0f57d8fb8aadb10fe
hadixcr <150840354+hadixcr@users.noreply.github.com>
2025-10-17 10:49:33 -0400

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,3 +1,28 @@
-public class Search {
+import java.util.Scanner;
 
+public class Search {
+	private String keyword;
+	private Pieces[] galleryKey;
+	
+	public void searchInput() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Search: ");
+		
+		keyword = keyboard.nextLine();
+		System.out.println("Search: " + keyword);
+		keyboard.close();
+	}
+	
+	public Pieces[] searchKey(String key) {
+		if (key.equals(null)) {
+			System.out.println("No keyword selected");
+			return galleryKey;
+		}
+		
+		return galleryKey;
+	}
+	
+	public String getKeyword() {
+		return keyword;
+	}
 }
